8c7e05dbf40cafaac6021dd09e7eaad60ef1c05e
natalie-abwoga <natalie.abwoga@strathmore.edu>
2025-07-14 07:27:58 +0300

    Update App.js

--- sky-connect-react/FrontEnd/src/App.js
+++ sky-connect-react/FrontEnd/src/App.js
@@ -5,8 +5,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-
-import ThankYou from './pages/ThankYou';            // ✅ Added
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ThankYou from './pages/ThankYou';
 import AvailableAirlines from './pages/AvailableAirlines';
 import FlightDetails from './pages/FlightDetails';
 import Payment from './pages/Payment';
@@ -19,7 +20,9 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
-                <Route path="/thankyou" element={<ThankYou />} />        {/* ✅ After Contact form */}
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/thankyou" element={<ThankYou />} />
                 <Route path="/airlines" element={<AvailableAirlines />} />
                 <Route path="/flight-details" element={<FlightDetails />} />
                 <Route path="/payment" element={<Payment />} />
